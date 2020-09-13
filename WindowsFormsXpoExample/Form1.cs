6cc667af0277989e1cb0859bb79dc3c92e5931bf
Jose Alberto Zavala Flores <jose.alberto.zavala.flores@gmail.com>
2020-09-13 12:28:42 -0700

    It's been added the database connection an a user creation example.

--- WindowsFormsXpoExample/Form1.cs
+++ WindowsFormsXpoExample/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +15,42 @@ namespace WindowsFormsXpoExample {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
+            LoadingDatabaseConnection();
+        }
+
+        /// <summary>
+        /// It does load database connection.
+        /// </summary>
+        public static void LoadingDatabaseConnection() {
+            try {
+                // Database connection string properties.
+                var server = @"ALBERTO-PC\SQL";
+                var dbUser = "testUser";
+                var dbPassword = "123456789";
+                var dbName = "XpoTestDatabase";
+
+                // It does create the database connection and bring back the unit of work that will be used.
+                var connection = MSSqlConnectionProvider.GetConnectionString(server, dbUser, dbPassword, dbName);
+                var dataLayerLocal = XpoDefault.GetDataLayer(connection, AutoCreateOption.DatabaseAndSchema);
+                var unitOfWork = new UnitOfWork(dataLayerLocal);
+
+                unitOfWork.Connect();
+
+                // It does update database structure.
+                unitOfWork.UpdateSchema();
+
+                // It does create a new user.
+                var user = new User(unitOfWork);
+                user.UserName = "AlbertoZavala";
+                user.Password = "qwerty";
+
+                // It does persist new user data in datbase.
+                unitOfWork.CommitChanges();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
---
+++ WindowsFormsXpoExample/User.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Data.Filtering;
+using DevExpress.Xpo;
+
+namespace WindowsFormsXpoExample {
+    public class User : XPObject {
+
+        /// <summary>
+        /// Enum definition for User status.
+        /// </summary>
+        public enum EStatus {
+            Enable,
+            Disable
+        }
+
+        /// <summary>
+        /// User Name
+        /// </summary>
+        private string mUserName;
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        private string mPassword;
+
+        /// <summary>
+        /// Indicate if user is enabled or disabled.
+        /// </summary>
+        private EStatus mStatus;
+
+        /// <summary>
+        /// Constructor that get the UnitOfWork and pass it to base clase
+        /// </summary>
+        /// <param name="unitOfWork"></param>
+        public User(UnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        public string UserName {
+            get { return mUserName; }
+            set { SetPropertyValue(nameof(UserName), ref mUserName, value); }
+        }
+
+        public string Password {
+            get { return mPassword; }
+            set { SetPropertyValue(nameof(Password), ref mPassword, value); }
+        }
+
+        public EStatus Status {
+            get { return mStatus; }
+            set { SetPropertyValue(nameof(mStatus), ref mStatus, value); }
+        }
+
+        public static class Properties {
+            public static OperandProperty Oid {
+                get { return new OperandProperty(nameof(Oid)); }
+            }
+
+            public static OperandProperty UserName {
+                get { return new OperandProperty(nameof(UserName)); }
+            }
+
+            public static OperandProperty Password {
+                get { return new OperandProperty(nameof(Password)); }
+            }
+
+            public static OperandProperty Status {
+                get { return new OperandProperty(nameof(Status)); }
+            }
+        }
+    }
+}
